ea7b246d211c1983070585450cdb00eb8738c6c5
Dawid Manikowski <dawid.manikowski@makeitright.pl>
2019-11-23 23:59:21 +0100

    Add kafka handling

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
+import json
+
 from flask import Flask
 from flask_restful import Api, Resource
+from kafka import (KafkaConsumer, KafkaProducer)
 
 app = Flask(__name__)
 api = Api(app)
 
 api_prefix = '/v1/api'
 component_prefix = '/consumables'
+
+producer = KafkaProducer(
+    bootstrap_servers=['kafka:9092'],
+    value_serializer=lambda x:
+    json.dumps(x).encode('utf-8')
+)
+
+consumer = KafkaConsumer(
+    'rest_api_response',
+    bootstrap_servers=['kafka:9092'],
+    auto_offset_reset='earliest',
+    enble_auto_commit=True,
+    group_id='my_group',
+    value_deserializer=lambda x:
+    json.loads(x).decode('utf-8')
+)
 
 
 class Consumable(Resource):
@@ -14,8 +33,18 @@ class Consumable(Resource):
             cons_id = int(cons_id)
         except ValueError:
             return {'error': ''}, 503
-        if cons_id == 999:
-            return
+        producer.send(
+            'rest_api_request',
+            {
+                "type": "item",
+                "subtype": "consumable",
+                "id": cons_id
+            }
+        )
+
+        ret = consumer.poll()
+
+        print(ret)
 
     def put(self, cons_id):
         pass
@@ -24,7 +53,13 @@ class Consumable(Resource):
         pass
 
 
+class Consumables(Resource):
+    def get(self):
+        return {}
+
+
 api.add_resource(Consumable, '/consumable/<cons_id>')
+api.add_resource(Consumables, '/consumables')
 
 if __name__ == '__main__':
     app.run(debug=True)
